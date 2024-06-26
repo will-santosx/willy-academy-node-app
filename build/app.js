d52e80e4978a5744b9e88d68c9511e713271b0a4
will-santosx <o.santxz@proton.me>
2024-06-26 12:30:38 -0400

    run build

---
+++ build/app.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const app_port = Number(process.env.PORT) || 8905;
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+app.get("/", (req, res) => {
+    res.status(201).send("<span>Funcionando!</span>");
+});
+app.listen(app_port, () => {
+    console.log("Servidor local rodando em: http://localhost:" + app_port);
+});
